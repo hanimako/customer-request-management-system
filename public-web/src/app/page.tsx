8f198b7ea3457eea1b3e33b99a506393fcbb3176
makoto <c2e7b8b6bfbf@gmail.com>
2026-07-06 18:51:34 +0900

    Add aplication registration prototype

--- public-web/src/app/page.tsx
+++ public-web/src/app/page.tsx
@@ -1,65 +1,57 @@
-import Image from "next/image";
+import { pool } from "@/lib/db";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main>
+      <div>
+        <h1>申請画面</h1>
+      </div>
+      <div>
+        <form action={createRequest}>
+          <div>
+            <label htmlFor="applicant_name">申請者名：</label>
+            <input id="applicant_name" name="applicant_name" type="text"></input>
+          </div>
+          <div>
+            <label htmlFor="applicant_address">住所：</label>
+            <input id="applicant_address" name="applicant_address" type="text"></input>
+          </div>
+          <div>
+            <label htmlFor="application_details">申請内容：</label>
+            <input id="application_details" name="application_details" type="text"></input>
+          </div>
+          <div>
+            <input type="submit" value="申請登録"></input>
+          </div>
+        </form>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    </main>
   );
 }
+
+async function createRequest(formDate: FormData) {
+  "use server";
+
+  const applicantName = formDate.get("applicant_name");
+  const applicantAddress = formDate.get("applicant_address");
+  const applicationDetails = formDate.get("application_details");
+
+  const result = await pool.query(
+    `
+      INSERT INTO applications (
+        application_datetime,
+        applicant_name,
+        applicant_address,
+        application_details 
+      ) 
+      VALUES (
+        CURRENT_TIMESTAMP,
+        $1,
+        $2,
+        $3
+      )
+    `,
+    [applicantName, applicantAddress, applicationDetails]
+  );
+  console.log(result.rows);
+};
