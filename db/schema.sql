bbfb41819f9d094589f93c391bf0fd4c0b792534
makoto <c2e7b8b6bfbf@gmail.com>
2026-07-09 08:13:00 +0900

    テーブル定義修正

--- db/schema.sql
+++ db/schema.sql
@@ -1,38 +1,38 @@
-CREATE TABLE applications (
-    application_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    application_datetime TIMESTAMP NOT NULL,
-    approved_flag BOOLEAN NOT NULL DEFAULT FALSE,
-    applicant_name VARCHAR(12) NOT NULL,
-    applicant_address VARCHAR(100) NOT NULL,
-    application_details VARCHAR(200) NOT NULL,
+CREATE TABLE requests (
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    request_datetime TIMESTAMP NOT NULL,
+    approved BOOLEAN NOT NULL DEFAULT FALSE,
+    requester_name VARCHAR(12) NOT NULL,
+    requester_address VARCHAR(100) NOT NULL,
+    content VARCHAR(200) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT applications_pk PRIMARY KEY (application_id)
+    CONSTRAINT requests_pk PRIMARY KEY (id)
 );
 
-COMMENT ON TABLE applications
+COMMENT ON TABLE requests
 IS '申請テーブル';
 
-COMMENT ON COLUMN applications.application_id
+COMMENT ON COLUMN requests.id
 IS '申請ID';
 
-COMMENT ON COLUMN applications.application_datetime
+COMMENT ON COLUMN requests.application_datetime
 IS '申請日時';
 
-COMMENT ON COLUMN applications.approved_flag
+COMMENT ON COLUMN requests.approved
 IS '承認済フラグ';
 
-COMMENT ON COLUMN applications.applicant_name
+COMMENT ON COLUMN requests.requester_name
 IS '申請者名';
 
-COMMENT ON COLUMN applications.applicant_address
+COMMENT ON COLUMN requests.requester_address
 IS '申請者住所';
 
-COMMENT ON COLUMN applications.application_details
+COMMENT ON COLUMN requests.content
 IS '申請内容';
 
-COMMENT ON COLUMN applications.created_at
+COMMENT ON COLUMN requests.created_at
 IS '新規登録日時';
 
-COMMENT ON COLUMN applications.updated_at
+COMMENT ON COLUMN requests.updated_at
 IS '更新日時';
--- db/table-definition.md
+++ db/table-definition.md
@@ -1,12 +1,13 @@
 # テーブル定義
 ## 申請テーブル
 ### 項目定義
-項目名          ：項目ID                ：制約
-1. 申請ID       ：application_id        ：INT PK NOTNULL　連番
-2. 申請日時     ：application_datetime  ：TIMESTAMP NOTNULL
-3. 承認済フラグ ：approved_flag         ：BOOLEAN NOTNULL　初期値false(未承認)
-4. 申請者名     ：applicant_name        ：VARCHAR(12) NOTNULL
-5. 申請者住所   ：applicant_address     ：VARCHAR(100) NOTNULL
-6. 申請内容     ：application_details   ：VARCHAR(200) NOTNULL
-7. 新規登録日時 ：created_at            ：TIMESTAMP NOTNULL
-8. 更新日時     ：updated_at            ：TIMESTAMP NOTNULL
+| 項目名 | 項目ID | 制約 |
+|---|---|---|
+| 申請ID | id | INT PK NOTNULL　連番 |
+| 申請日時 | request_datetime | TIMESTAMP NOTNULL |
+| 承認済フラグ | approved | BOOLEAN NOTNULL　初期値false(未承認) |
+| 申請者名 | requester_name | VARCHAR(12) NOTNULL |
+| 申請者住所 | requester_address | VARCHAR(100) NOTNULL |
+| 申請内容 | content | VARCHAR(200) NOTNULL |
+| 新規登録日時 | created_at | TIMESTAMP NOTNULL |
+| 更新日時 | updated_at | TIMESTAMP NOTNULL |
